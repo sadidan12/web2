b0f6d8d0d2f3bffd7c839e65a7c8999b722f5bfb
sadidan12 <79885975+sadidan12@users.noreply.github.com>
2021-03-02 15:11:53 +0700

    Update pert1_latihan3.php

--- pert1_latihan3.php
+++ pert1_latihan3.php
@@ -2,19 +2,19 @@
 <head><title>Variabel Static</title></head>
 <body><h1>Variabel Static</h1>
 <?php
- Function test()
- {
- Static $a=0; // dengan static
- Echo “Nilai a : “;
- Echo $a; 
- Echo “<br>”;
- $a++;
- }
- Test();
- Test();
- Test();
- Test();
- Test();
+Function test()
+{
+Static $a=0; // dengan static
+Echo "Nilai a : ";
+Echo $a;
+Echo "<br>";
+$a++;
+}
+Test();
+Test();
+Test();
+Test();
+Test();
 ?>
 </body>
 </html>

09cbe2719517e6267b24c128d579f5fc4a1171a2
sadidan12 <79885975+sadidan12@users.noreply.github.com>
2021-03-03 15:47:25 +0700

    Create pert2_latihan2.php

---
+++ pert2_latihan2.php
@@ -0,0 +1,10 @@
+<?php
+// inisiasi variable yang digunakan
+// nama peralatan
+$brg1 = "Buku";
+$brg2 = "Mouse";
+$brg3 = "FlashDisk";
+$brg4 = "Pulpen";
+// harga per unit peralatan
+$harga1 = 17500;
+$harga2 = 30000; 
